23f197aec2c7e314eb3c718e03199ca913049dda
Pranav Krishnan <pranavkrish2005.gmail.com>
2024-08-31 20:21:26 -0400

    made changes and added sign in

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { connectToDB } from '@utils/database';
+import User from '@models/user';
 
 const handler = NextAuth({
     providers: [
@@ -7,17 +9,43 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
-    ], async session({ session }) {
+    ],
+    callbacks: {
+        async session({ session }) {
+            await connectToDB();
+            const sessionUser = await User.findOne({
+                email: session.user.email
+            });
 
-    },
-    async signIn({ profile }) {
-        try {
+            session.user.id = sessionUser._id.toString();
 
-        } catch (error) {
-            
+            return session;
+        },
+        async signIn({ profile }) {
+            try {
+                await connectToDB();
+
+                // check if a user already exists
+                const userExists = await User.findOne({
+                    email: profile.email,
+                });
+
+                // if not, create a user and save to database
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture
+                    });
+                }
+
+                return true;
+            } catch (error) {
+                console.log(error);
+                return false;
+            }
         }
-    },
-})
+    }
+});
 
-// not something we usually do. We either do a get or a post. Not both. For next auth this is how to do it
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,17 +9,17 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const {data: session} = useSession();
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
       setProviders(response);
     }
 
-    setProviders();
+    setUpProviders();
   }, [setProviders])
 
   return (
@@ -34,7 +34,7 @@ const Nav = () => {
 
       {/* Desktop navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href='/create-prompt' className="black_btn">
               Create Post
@@ -45,7 +45,7 @@ const Nav = () => {
             </button>
 
             <Link href='/profile'>
-              <Image src="/assets/images/logo.svg"
+              <Image src={session?.user.image}
                 width={37}
                 height={37} className="rounded-full" />
             </Link>
@@ -69,9 +69,9 @@ const Nav = () => {
 
       {/* Mobile navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
-            <Image src="/assets/images/logo.svg"
+            <Image src={session?.user.image}
               width={37}
               height={37} className="rounded-full" 
               onClick={() => {setToggleDropdown((prev) => !prev)}} />
